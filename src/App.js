5f670835abef7082be48d19c336aa7db3f468991
Raidanor <silverstallion30@gmail.com>
2023-10-13 07:09:12 +0400

    Added Calendar Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './components/Navbar.js'
+import CalendarComponenent from './components/CalendarComponent.js'
+
 
 function App() {
   return (
     <div>
         <Navbar />
-        
+        <CalendarComponenent />
         
         <div class="container-fluid bg-custom">
             
