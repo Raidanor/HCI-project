7c4bb19861a849191a1eed7561ab06a1829e9a16
Raidanor <silverstallion30@gmail.com>
2023-10-19 03:03:13 +0400

    broken calendar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
+import { CalendarComponent } from "./pages/CalendarComponent.jsx"
 import Secert  from "./pages/Secert";
 import "react-toastify/dist/ReactToastify.css"
 
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/Register" element={<Register/>}/>
         <Route exact path="/Login" element={<Login/>}/>
-        <Route exact path="/" element={<Secert/>}/>
+        <Route exact path="/" element={<CalendarComponent/>}/>
       </Routes>
     </BrowserRouter>
   );
