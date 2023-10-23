e594c2b183ccfbc85e57e4fc6ffe2a21dcef915a
JFloyd11 <145620345+JFloyd11@users.noreply.github.com>
2023-10-23 19:12:59 -0300

    profilepageworking

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link,useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
+import profile_icon from '../assets/astronaut.png'
 
 export default function Profile() {
     const nav = useNavigate();
@@ -35,6 +36,21 @@ export default function Profile() {
 
     return (
         <div className="private">
+            
+            <div className='upc'>
+            <div className='gradiant'></div>
+            <div className='profile-down'>
+                <img src={profile_icon} alt=""/>
+                <div className='profile-title'>Hi, Akbar!</div>
+                <div className='profile-description'>
+                Descriptions
+                </div>
+                <button className="button">Rewards</button>
+                <button className="button">Calendar</button>
+                <button className="button">Logout</button>
+            </div>   
+        </div>
+            
             <h1>Logged in on Profile page</h1>
             <Link to="/Login">Back to Login</Link>
             <br/>
