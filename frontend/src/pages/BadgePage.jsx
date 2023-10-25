7acdacdc57461caa663fac711a1486eebc460765
amazingtoludada <dadatolu@gmail.com>
2023-10-25 20:41:48 -0300

    update

--- frontend/src/pages/BadgePage.jsx
+++ frontend/src/pages/BadgePage.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Link,useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
+import "./BadgePage.css";
+import badge1 from '../assets/Picture1.png';
+import badge2 from '../assets/Picture2.png';
+import badge3 from '../assets/Picture3.png';
 
 export default function BadgePage() {
     const nav = useNavigate();
@@ -11,12 +15,17 @@ export default function BadgePage() {
         nav("/Login"); 
     };
 
-    useEffect(() => {
-        const verifyUser = async () => {
-            if (!cookies.jwt) {
+    useEffect(() =>
+    {
+        const verifyUser = async () =>
+        {
+            if (!cookies.jwt)
+            {
                 nav("/Login");
-            } else {
-                try {
+            } else
+            {
+                try
+                {
                     const response = await axios.post("http://localhost:3500", {}, {
                         withCredentials: true,
                     });
@@ -35,11 +44,11 @@ export default function BadgePage() {
 
     return (
         <div className="private">
-            <h1>Logged in on BadgePage!</h1>
-            <Link to="/Login">Back to Login</Link>
-            <br/>
-            <Link to="/">Back to logout button</Link>
-            <button onClick={logOut}>Log Out</button>
+            <div className="img">
+                <img src={badge1} alt=""/>
+                <img src={badge2} alt=""/>
+                <img src={badge3} alt=""/>
+            </div>
         </div>
     );
 }
