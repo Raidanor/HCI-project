935f1d44dab570a6c51528310201aff9b2f11eea
ShaUpshaw <shaupshaw@gmail.com>
2023-10-25 19:19:43 -0300

    Working fine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,35 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
-import Navbar from './components/Navbar.js'
 import { Register } from "./pages/Register";
 import Profile from './pages/Profile';
 import BadgePage from './pages/BadgePage';
-import CalendarComponent from "./pages/CalendarComponent.jsx";
-import  Secert   from "./pages/Secert";
-import "react-toastify/dist/ReactToastify.css"
+import CalendarComponent from "./components/CalendarComponent.js";
+import Navbar from './components/Navbar';
+import Secert from "./pages/Secert";
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from 'react-toastify';
 import React, { useEffect, useState } from "react";
 
 
 
-function App() {
-
+function App()
+{
   return (
-    <BrowserRouter>
-      <Navbar />
-      <ToastContainer/>
-      <Routes>
-        <Route exact path="/Register" element={<Register/>}/>
-        <Route exact path="/Login" element={<Login/>}/>
-        <Route exact path='/' element={<Secert />}/>
-        <Route exact path='/Profile' element={<Profile />}/>
-        <Route exact path='/Profile/BadgePage' element={<BadgePage />}/>
-        <Route exact path='/Profile/Calendar' element={<CalendarComponent />}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+        <BrowserRouter>
+        <Navbar />
+            <ToastContainer/>
+            <Routes>
+                <Route exact path="/Register" element={<Register/>}/>
+                <Route exact path="/Login" element={<Login/>}/>
+                <Route exact path='/' element={<Secert />}/>
+                <Route exact path='/Profile' element={<Profile />}/>
+                <Route exact path='/Profile/BadgePage' element={<BadgePage />}/>
+                <Route exact path='/Profile/Calendar' element={<CalendarComponent />}/>
+            </Routes>
+        </BrowserRouter>
+    </>
   );
 }
 
