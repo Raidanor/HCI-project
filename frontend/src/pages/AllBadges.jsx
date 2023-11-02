3229c877735625c65c117fc34918b7a610ee990b
ShaUpshaw <shaupshaw@gmail.com>
2023-11-02 20:59:01 -0300

    more changes to allbadges

--- frontend/src/pages/AllBadges.jsx
+++ frontend/src/pages/AllBadges.jsx
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
-import "./hoverStyles.css"; //new
+import "./hoverStyles.css";
 import "./myCSS.css";
-import { Tooltip } from "./Tooltip";
-
 import Picture1 from "../assets/Picture1.png";
 import Picture2 from "../assets/Picture2.png";
 import Picture3 from "../assets/Picture3.png";
 
-
 export default function AllBadges() {
     const nav = useNavigate();
-    const [cookies, removeCookie] = useCookies([]); 
+    const [cookies, removeCookie] = useCookies([]);
     const logOut = () => {
-        removeCookie("jwt"); 
-        nav("/Login"); 
+        removeCookie("jwt");
+        nav("/Login");
     };
 
     useEffect(() => {
@@ -30,7 +27,8 @@ export default function AllBadges() {
                     });
 
                     if (!response.data.status) {
-                        logOut(); // Call the logOut function to remove the cookie and navigate to the login page
+                        logOut(); 
+                        // Call the logOut function to remove the cookie and navigate to the login page
                     }
                 } catch (error) {
                     console.error("Error:", error);
@@ -40,78 +38,56 @@ export default function AllBadges() {
 
         verifyUser();
     }, [cookies.jwt, nav]);
-    
 
     return (
-        
-        <div className="container">
-            {/* <div className="row">
-                <div className="col-4"> */}
-                    {/* <h2 className="level">Level 1</h2> */}
-                {/* </div>
-                <div className="col-4">
+        <div className="badge-container">
+            <div className="row">
+                <div className="col-4 text-center">
+                    <h2 className="level">Level 1</h2>
+                    <img src={Picture1} alt="Badge1" className="grey badge-grey hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one day.</div>
+                </div>
+                <div className="col-4 text-center">
                     <h2 className="level">Level 2</h2>
+                    <img src={Picture1} alt="Badge1" className="badge-colored hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one day seven times.</div>
                 </div>
-                <div className="col-4">
+                <div className="col-4 text-center">
                     <h2 className="level">Level 3</h2>
-                </div>
-            </div> */}
-
-            <div className="row justify-content-around py-3">
-                <div className="col-3"><h1>Level 1</h1></div>
-                <div className="col-3"><h1>Level 2</h1></div>
-                <div className="col-3"><h1>Level 3</h1></div>
-            </div>
-
-            <div className="row">
-                
-                <div className="col-4">
-                <img src = {Picture1} alt="Badge1" className="grey badge-grey rounded-circle" /> 
-                <div className="py-3">Unlock this badge once you have completed all you tasks for one day.</div>
-                
-                </div>
-                
-                
-                <div className="col-4">
-                    <img src = {Picture1} alt="Badge1" className="badge-colored rounded-circle"/> 
-                    <div className="py-3">Unlock this badge once you have completed all you tasks for one day seven times.</div>
-                </div>
-                <div className="col-4">
-                    <img src = {Picture1} alt="Badge1" className="badge-rounded rounded-circle border-glowing" />
-                    <div className="py-3">Unlock this badge once you have completed all you tasks for one day thirty times.</div>
+                    <img src={Picture1} alt="Badge1" className="badge-rounded rounded-circle border-glowing hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one day thirty times.</div>
                 </div>
             </div>
             <br />
             <div className="row">
-                <div className="col-4">
-                    <img src = {Picture2} alt="Badge2" className="grey badge-grey rounded-circle" />
-                    <div className="py-3">Unlock this badge once you have completed all you tasks for one week.</div>
+                <div className="col-4 text-center">
+                    <img src={Picture2} alt="Badge2" className="grey badge-grey hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one week.</div>
                 </div>
-                <div className="col-4">
-                    <img src = {Picture2} alt="Badge2" className="badge-colored rounded-circle" /> 
-                    <div className="py-3">Unlock this badge once you have completed all you tasks for one week ten times.</div>
+                <div className="col-4 text-center">
+                    <img src={Picture2} alt="Badge2" className="badge-colored hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one week ten times.</div>
                 </div>
-                <div className="col-4">
-                    <img src = {Picture2} alt="Badge2" className="badge-rounded rounded-circle border-glowing" />
-                    <div className="py-3">Unlock this badge once you have completed all you tasks for one week 30 times.</div>
+                <div className="col-4 text-center">
+                    <img src={Picture2} alt="Badge2" className="badge-rounded rounded-circle border-glowing hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one week 30 times.</div>
                 </div>
             </div>
             <br />
             <div className="row">
-                <div className="col-4">
-                    <img src = {Picture3} alt="Badge3" className="grey badge-grey rounded-circle" /> 
-                    <div className="py-3">Unlock this badge once you have completed all you tasks for one month.</div>
+                <div className="col-4 text-center">
+                    <img src={Picture3} alt="Badge3" className="grey badge-grey hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one month.</div>
                 </div>
-                <div className="col-4">
-                    <img src = {Picture3} alt="Badge3" className="badge-colored rounded-circle"/> 
-                    <div className="py-3">Unlock this badge once you have completed all you tasks for one month six times.</div>
+                <div className="col-4 text-center">
+                    <img src={Picture3} alt="Badge3" className="badge-colored hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one month six times.</div>
                 </div>
-                <div className="col-4">
-                    <img src = {Picture3} alt="Badge3" className="badge-rounded rounded-circle border-glowing" />
-                    <div className="py-3">Unlock this badge once you have completed all you tasks for one month twelve times.</div>
+                <div className="col-4 text-center">
+                    <img src={Picture3} alt="Badge3" className="badge-rounded rounded-circle border-glowing hover-effect" />
+                    <div className="badge-text">Receive this badge once you've completed all your tasks for one month twelve times.</div>
                 </div>
             </div>
-            
         </div>
     );
 }
