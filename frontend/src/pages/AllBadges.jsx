a8a379e6666626ed54c339c9ccad270a6d61767f
JFloyd11 <145620345+JFloyd11@users.noreply.github.com>
2023-11-02 17:22:32 -0300

    styledallbadgespage

--- frontend/src/pages/AllBadges.jsx
+++ frontend/src/pages/AllBadges.jsx
@@ -4,6 +4,7 @@ import { useCookies } from "react-cookie";
 import axios from "axios";
 import "./hoverStyles.css"; //new
 import "./myCSS.css";
+import { Tooltip } from "./Tooltip";
 
 import Picture1 from "../assets/Picture1.png";
 import Picture2 from "../assets/Picture2.png";
@@ -39,42 +40,71 @@ export default function AllBadges() {
 
         verifyUser();
     }, [cookies.jwt, nav]);
+    
 
     return (
+        
         <div className="container">
+            {/* <div className="row">
+                <div className="col-4"> */}
+                    {/* <h2 className="level">Level 1</h2> */}
+                {/* </div>
+                <div className="col-4">
+                    <h2 className="level">Level 2</h2>
+                </div>
+                <div className="col-4">
+                    <h2 className="level">Level 3</h2>
+                </div>
+            </div> */}
             <div className="row">
+                
                 <div className="col-4">
-                    <img src = {Picture1} alt="Badge1" className="grey badge-grey" /> 
+                <h2 className="level">Level 1</h2>
+                <img src = {Picture1} alt="Badge1" className="grey badge-grey" /> 
+                <div>Recieve this badge once you have completed all you tasks for one day.</div>
+                
                 </div>
+                
+                
                 <div className="col-4">
+                    <h2 className="level">Level 2</h2>
                     <img src = {Picture1} alt="Badge1" className="badge-colored"/> 
+                    <div>Recieve this badge once you have completed all you tasks for one day seven times.</div>
                 </div>
                 <div className="col-4">
+                    <h2 className="level">Level 3</h2>
                     <img src = {Picture1} alt="Badge1" className="badge-rounded rounded-circle border-glowing" />
+                    <div>Recieve this badge once you have completed all you tasks for one day thirty times.</div>
                 </div>
             </div>
             <br />
             <div className="row">
                 <div className="col-4">
                     <img src = {Picture2} alt="Badge2" className="grey badge-grey" />
+                    <div>Recieve this badge once you have completed all you tasks for one week.</div>
                 </div>
                 <div className="col-4">
                     <img src = {Picture2} alt="Badge2" className="badge-colored" /> 
+                    <div>Recieve this badge once you have completed all you tasks for one week ten times.</div>
                 </div>
                 <div className="col-4">
                     <img src = {Picture2} alt="Badge2" className="badge-rounded rounded-circle border-glowing" />
+                    <div>Recieve this badge once you have completed all you tasks for one week 30 times.</div>
                 </div>
             </div>
             <br />
             <div className="row">
                 <div className="col-4">
                     <img src = {Picture3} alt="Badge3" className="grey badge-grey" /> 
+                    <div>Recieve this badge once you have completed all you tasks for one month.</div>
                 </div>
                 <div className="col-4">
                     <img src = {Picture3} alt="Badge3" className="badge-colored"/> 
+                    <div>Recieve this badge once you have completed all you tasks for one month six times.</div>
                 </div>
                 <div className="col-4">
                     <img src = {Picture3} alt="Badge3" className="badge-rounded rounded-circle border-glowing" />
+                    <div>Recieve this badge once you have completed all you tasks for one month twelve times.</div>
                 </div>
             </div>
             
