a09595d83710a4055817625c53a257cb746ab95b
ShaUpshaw <shaupshaw@gmail.com>
2023-11-02 13:04:44 -0300

    CRUD added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar"
 import { Login } from "./pages/Login";
